4d3aa3a0a47465ddd7fac7db8753e35877443b4e
Rajesh Gajra <rajeshgajra19889@gmail.com>
2026-09-12 22:10:39 +0530

    updated backend url

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // TODO: replace with your real hosted backend URL
-  api: 'https://your-backend-domain',
+  api: 'https://kanban-backend-2y8y.onrender.com',
 };
